2f3864231f454a793880958774ff45eb1137490b
maxlukyrabbit <maxlukyrabbit@yandex.ru>
2023-09-08 09:26:24 +0300

    08.09.1013

---
+++ 2-laba/1/Program.cs
@@ -0,0 +1,176 @@
+﻿class Program
+{
+    static void Main(string[] args)
+
+    {
+
+        Console.WriteLine("а-------------------------");
+
+        Random rnd = new Random();
+        int a = rnd.Next(1, 10);
+        int[] b = new int[a];
+        int[] c = new int[a];
+        int sum1 = 0;
+        int sum2 = 0;
+
+        for (int i = 0; i < a; i++)
+        {
+            b[i] = rnd.Next(1, 30);
+        }
+
+        for (int j = 0; j < a; j++)
+        {
+            c[j] = b[j] * 78;
+        }
+        Console.WriteLine("б-------------------------");
+        for (int j = 0; j < a; j++)
+        {
+            Console.Write(b[j] + " ");
+            sum1 += b[j];
+        }
+
+        Console.WriteLine();
+
+        for (int j = 0; j < a; j++)
+        {
+            Console.Write(c[j] + " ");
+            sum2 += c[j];
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("в-------------------------");
+
+        Console.WriteLine(sum1);
+        Console.WriteLine(sum2);
+
+        Console.WriteLine("г-------------------------");
+
+
+        Console.WriteLine($"max first: {b.Max()}, min first: {b.Min()}");
+        Console.WriteLine($"max second: {c.Max()}, min second: {c.Min()}");
+
+        Console.WriteLine("д-------------------------");
+
+
+        int p = Convert.ToInt32(Console.ReadLine());
+
+        if (b.Contains(p))
+        {
+            Console.WriteLine("First true");
+        }
+        else
+        {
+            Console.WriteLine("First false");
+        }
+
+        if (c.Contains(p))
+        {
+            Console.WriteLine("Second true");
+        }
+        else
+        {
+            Console.WriteLine("Second false");
+        }
+
+        Console.WriteLine("е-------------------------");
+
+        var b50 = from elem in b
+                  where elem > 1 && elem < 50
+                  select elem;
+
+        var c40 = from elem in c
+                  where elem > 70 && elem < 500
+                  select elem;
+
+        Console.WriteLine("Элементы 1 массива, которые меньше 50 и больше 1");
+        foreach (var element in b50)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("Элементы 2 массива, которые меньше 500 и больше 70");
+
+        foreach (var element in b50)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("ж-------------------------");
+
+
+        int[] all = b.Where(element => c.Contains(element)).ToArray();
+        if (all.Length == 0)
+        {
+            Console.WriteLine("Таких элементов нет");
+        }
+        else
+        {
+            foreach (var element in all)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        Console.WriteLine("з-------------------------");
+
+        int[] combinedArray = b.Concat(c).ToArray();
+
+        Console.WriteLine("Объединенный массив:");
+        foreach (var element in combinedArray)
+        {
+            Console.Write(element + " ");
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("и-------------------------");
+
+
+        if (all.Length == 0)
+        {
+            Console.WriteLine("Пересечения нет");
+        }
+        else
+        {
+            foreach (var element in all)
+            {
+                Console.WriteLine(element);
+            }
+        }
+
+        Console.WriteLine("");
+        Console.WriteLine("к-------------------------");
+
+        var result1 = combinedArray.OrderBy(t => t);
+         Console.WriteLine("По возрастанию");
+        foreach (int t in result1)
+            Console.Write($"{t} ");
+
+        Console.WriteLine("");
+        Console.WriteLine("к.1-------------------------");
+
+        var result2 = combinedArray.OrderByDescending(t => t);
+        Console.WriteLine("По убыванию");
+        foreach (int t in result2)
+            Console.Write($"{t} ");
+
+        Console.WriteLine("");
+        Console.WriteLine("к.2-------------------------");
+
+    }
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
---
+++ 2-laba/2/Program.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Student> students = new List<Student>
+        {
+            new Student { studentnumber = 1, Surname = "Sluchaev", name = "Maxim", patronymic = "Konstantinovich", yearbirth = "2005", studentclass = "9V", averagescore = 5.0},
+            new Student { studentnumber = 2, Surname = "Spiridonov", name = "Evgeniy", patronymic = "Andreevich", yearbirth = "2005", studentclass = "9V", averagescore = 4.99},
+            new Student { studentnumber = 3, Surname = "Patkov", name = "Dmitry", patronymic = "Andreevich", yearbirth = "2009", studentclass = "9A", averagescore = 4.0},
+            new Student { studentnumber = 4, Surname = "Shavaliev", name = "Timur", patronymic = "Maratovich", yearbirth = "2005", studentclass = "9V", averagescore = 4.0},
+            new Student { studentnumber = 5, Surname = "Funtova", name = "Sofi", patronymic = "Pavlovna", yearbirth = "2005", studentclass = "9V", averagescore = 4.4}
+        };
+        getinfo(students);
+
+        System.Console.WriteLine($"_______________________________");
+
+        var ListMax = students.Max(p => p.averagescore);
+        var ListMin = students.Min(p => p.averagescore);
+
+        System.Console.WriteLine($"Максимальный балл {ListMax}");
+        System.Console.WriteLine($"Минимальный балл {ListMin}");
+
+        System.Console.WriteLine($"_______________________________");
+
+
+        var result9V = students
+            .Where(p => p.studentclass == "9A");
+
+        foreach (var student in result9V)
+        {
+            Console.WriteLine($"Номер: {student.studentnumber}");
+            Console.WriteLine($"Фамилия: {student.Surname}");
+            Console.WriteLine($"Имя: {student.name}");
+            Console.WriteLine($"Отчество: {student.patronymic}");
+            Console.WriteLine($"Год рождения: {student.yearbirth}");
+            Console.WriteLine($"Класс: {student.studentclass}");
+            Console.WriteLine($"Средний балл: {student.averagescore}");
+            Console.WriteLine();
+        }
+
+        System.Console.WriteLine($"_______________________________");
+
+        var result2009 = students
+            .Where(p => p.yearbirth == "2009");
+
+        foreach (var student in result2009)
+        {
+            Console.WriteLine($"Номер: {student.studentnumber}");
+            Console.WriteLine($"Фамилия: {student.Surname}");
+            Console.WriteLine($"Имя: {student.name}");
+            Console.WriteLine($"Отчество: {student.patronymic}");
+            Console.WriteLine($"Год рождения: {student.yearbirth}");
+            Console.WriteLine($"Класс: {student.studentclass}");
+            Console.WriteLine($"Средний балл: {student.averagescore}");
+            Console.WriteLine();
+        }
+
+        System.Console.WriteLine($"_______________________________");
+
+         var gr = students.GroupBy(args => args.studentclass); 
+
+        foreach (var student in gr)
+        {
+            Console.WriteLine($"{student.Key} {student.Average(x => x.averagescore)}");
+        }
+
+
+
+        System.Console.WriteLine($"_______________________________");
+
+
+        var FIO = students.Select(p => new { p.Surname, p.studentnumber });
+
+        foreach (var student in FIO)
+        {
+            Console.WriteLine($"Фамилия: {student.Surname}");
+            Console.WriteLine($"Номер: {student.studentnumber}");
+            Console.WriteLine();
+        }
+
+        System.Console.WriteLine($"_______________________________");
+
+        var sort = students.OrderBy(args => args.averagescore).ToList();
+
+        foreach (var student in sort)
+        {
+            Console.WriteLine($"Номер: {student.studentnumber}");
+            Console.WriteLine($"Фамилия: {student.Surname}");
+            Console.WriteLine($"Имя: {student.name}");
+            Console.WriteLine($"Отчество: {student.patronymic}");
+            Console.WriteLine($"Год рождения: {student.yearbirth}");
+            Console.WriteLine($"Класс: {student.studentclass}");
+            Console.WriteLine($"Средний балл: {student.averagescore}");
+            Console.WriteLine();
+        }
+
+        System.Console.WriteLine($"_______________________________");
+
+
+
+
+
+
+    }
+
+    static void getinfo(List<Student> students)
+    {
+
+        foreach (Student student in students)
+        {
+            Console.WriteLine($"Номер: {student.studentnumber}");
+            Console.WriteLine($"Фамилия: {student.Surname}");
+            Console.WriteLine($"Имя: {student.name}");
+            Console.WriteLine($"Отчество: {student.patronymic}");
+            Console.WriteLine($"Год рождения: {student.yearbirth}");
+            Console.WriteLine($"Класс: {student.studentclass}");
+            Console.WriteLine($"Средний балл: {student.averagescore}");
+            Console.WriteLine();
+        }
+    }
+}
+
+class Student
+{
+    public int studentnumber { get; set; }
+    public string Surname { get; set; }
+    public string name { get; set; }
+    public string patronymic { get; set; }
+    public string yearbirth { get; set; }
+    public string studentclass { get; set; }
+    public double averagescore { get; set; }
+}
